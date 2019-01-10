9890860eaae319ba769ae996bda91b94bcdf2b5c
rmanikandan441 <42027847+rmanikandan441@users.noreply.github.com>
2019-01-10 12:41:28 +0530

    Create prgrm4.py

---
+++ prgrm4.py
@@ -0,0 +1,5 @@
+letter = input("Enter a character: ")
+if((letter>='a' and letter<= 'z') or (letter>='A' and letter<='Z')):
+    print(letter, "is an Alphabet")
+else:
+    print(letter, "is not an Alphabet")
